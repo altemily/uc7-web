bbfb32ca4a6183a8445b37e59bb844953b509c98
altemily <altemilyc@gmail.com>
2025-02-19 13:53:50 -0300

    add: Adicionar aula 07 ao repositório.

---
+++ aula7-DOM/script.js
@@ -0,0 +1,14 @@
+let titulo = document.getElementById("titulo");
+console.log(typeof titulo);
+console.log(titulo.innerText);
+
+function verificarInput(){
+  let input = document.getElementById('nome');
+  // console.log(input);
+  if(nome.value.length < 5){
+    nome.className = 'erro';
+  }else{
+    nome.className = '';
+  }
+
+}
