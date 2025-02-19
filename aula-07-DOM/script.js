cfacb2788d1cbb9d6623cd80e2220bb702fac378
altemily <altemilyc@gmail.com>
2025-02-19 14:11:40 -0300

    add: Adicionar aula 07 ao repositório.

--- aula-07-DOM/script.js
+++ aula-07-DOM/script.js
@@ -1,9 +1,9 @@
-let titulo = document.getElementById("titulo");
+//let titulo = document.getElementById("titulo");
 console.log(typeof titulo);
 console.log(titulo.innerText);
 
 function verificarInput(){
-  let input = document.getElementById('nome');
+  //let input = document.getElementById('nome');
   // console.log(input);
   if(nome.value.length < 5){
     nome.className = 'erro';
